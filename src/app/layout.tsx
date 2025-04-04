cadb3330f2d985ddda524c3ad99563a6b07e20e2
apricityverse <felicitywellnesshub@gmail.com>
2025-04-04 10:50:45 +0530

    commented the clerk provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    //<ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           {children} <ToastContainer position="bottom-right" theme="dark" />
         </body>
       </html>
-    </ClerkProvider>
+    //</ClerkProvider>
   );
 }
