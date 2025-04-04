1433cddb325eff1a47365f8040001454062e1ea9
apricityverse <felicitywellnesshub@gmail.com>
2025-04-04 10:49:30 +0530

    rennamed to Next Photon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Next Photon EduTech Management Dashboard",
+  description: "Next Photon EduTech Management System",
 };
 
 export default function RootLayout({
