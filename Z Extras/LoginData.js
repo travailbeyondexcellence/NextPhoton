2e67d990a97e9ddd802f68151bd8e18debbc1bcd
travailbeyondexcellence <zen@paathshala.world>
2025-05-05 20:59:17 +0530

    Refactor: update test login data structure and add comments for clarity

--- Z Extras/LoginData.js
+++ Z Extras/LoginData.js
@@ -1,7 +1,46 @@
 
 {
-    username: testStudent1
-    password: testStudent1
+
+
+    // Clerk Dev Environment
+    // Test Admin Login Data
+
+    // username: admin1
+    // password: testAdmin1@99
+    // email: admin1@nextphoton.com
+}
+
+
+{
+
+
+    // Clerk Dev Environment
+    // Test Student Login Data
+
+
+    // username: testStudent1
+    // password: testStudent1@99
     // email: teststudent1@nextphoton.com
 }
 
+{
+
+
+    // Clerk Dev Environment
+    // Test Parent Login Data
+
+    // username: testParent1
+    // password: testParent1@99
+    // email: testparent1@nextphoton.com
+}
+
+
+{
+
+    // Clerk Dev Environment
+    // Test Teacher Login Data
+
+    // username: testTeacher1
+    // password: testTeacher1@99
+    // email: testTeacher1@nextphoton.com
+}
