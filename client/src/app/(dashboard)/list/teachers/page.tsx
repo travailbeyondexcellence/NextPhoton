d0209381bc3dd810b073f0bcf5a1103cbb10a358
travailbeyondexcellence <zen@paathshala.world>
2025-05-05 23:07:07 +0530

    Enhance dark mode support in TeacherListPage component

--- client/src/app/(dashboard)/list/teachers/page.tsx
+++ client/src/app/(dashboard)/list/teachers/page.tsx
@@ -145,17 +145,17 @@ const TeacherListPage = async ({
   ]);
 
   return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+    <div className="bg-white dark:bg-gray-900 p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold dark:text-gray-100">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow dark:bg-gray-700">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow dark:bg-gray-700">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
