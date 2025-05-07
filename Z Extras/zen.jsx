bacbb2c296f95d7897209de463e4f1cf4c1eb1cd
travailbeyondexcellence <zen@paathshala.world>
2025-05-07 22:51:24 +0530

    Add initial zen component and Zenith function for name display

---
+++ Z Extras/zen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const zen = () => {
+  return (
+    <div>zen</div>
+  )
+}
+
+export default zen
+
+
+const Zenith = (a, b) => {
+
+    return "My name is Zen";
+}
+
+
