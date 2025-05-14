d3615493dca1f420a75519b5dcfcf7e7f18b7dc0
travailbeyondexcellence <zen@paathshala.world>
2025-05-14 11:19:10 +0530

    directory sturcture setup

---
+++ client/src/app/(dashboard)/employee/page.tsx
@@ -0,0 +1,14 @@
+
+const EmployeePage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+     
+    </div>
+  );
+};
+
+export default EmployeePage;
