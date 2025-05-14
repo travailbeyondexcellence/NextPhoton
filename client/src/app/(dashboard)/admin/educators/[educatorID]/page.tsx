12dd9eba725c715cbb54385a6bd86357c80ced25
travailbeyondexcellence <zen@paathshala.world>
2025-05-14 20:04:14 +0530

    feat: create EduCatorProfilePage component for educator details display

---
+++ client/src/app/(dashboard)/admin/educators/[educatorID]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import EducatorProfile_forAdmin from '@/components/EducatorProfile_forAdmin'
+
+const EduCatorProfilePage = () => {
+  return (
+      <div>
+          <span className='text-3xl font-semibold dark:text-gray-500'>EduCator Profile Page
+          </span>
+          
+          <EducatorProfile_forAdmin />
+          
+        </div>
+  )
+}
+
+export default EduCatorProfilePage
