792970c649d566805674f060c3cc0dcd9c8e012a
travailbeyondexcellence <zen@paathshala.world>
2025-05-14 13:36:07 +0530

    layout file is correct

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -9,17 +9,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
+    <div className="grow flex flex-col min-w-0 bg-blue-100">
     <SidebarProvider>
       <div className="flex min-h-screen">
         {/* Sidebar */}
         <DashboardSidebar />
 
         {/* Main Content Area */}
-        <div className="flex-1 flex flex-col min-w-0">
+        <div className="grow flex flex-col min-w-0">
           <DashboardNavbar onMenuClick={() => setIsMobileMenuOpen(true)} />
           <main className="flex-1 p-6">{children}</main>
         </div>
       </div>
-    </SidebarProvider>
+      </SidebarProvider>
+      </div>
   );
 }
