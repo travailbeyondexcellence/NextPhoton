3de3603ae2d306b9498b154e2c85a3db97188e4c
travailbeyondexcellence <zen@paathshala.world>
2025-05-14 20:46:10 +0530

    style: update EducatorProfile_forAdmin component for improved UI and accessibility

--- client/src/app/(dashboard)/admin/educators/[educatorID]/page.tsx
+++ client/src/app/(dashboard)/admin/educators/[educatorID]/page.tsx
@@ -5,8 +5,7 @@ import EducatorProfile_forAdmin from '@/components/EducatorProfile_forAdmin'
 const EduCatorProfilePage = () => {
   return (
       <div>
-          <span className='text-3xl font-semibold dark:text-gray-500'>EduCator Profile Page
-          </span>
+       
           
           <EducatorProfile_forAdmin />
           
