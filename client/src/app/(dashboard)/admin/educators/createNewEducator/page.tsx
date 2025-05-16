50b27b214f5a41030ddc4aa78784fd185d003af3
travailbeyondexcellence <zen@paathshala.world>
2025-05-17 00:28:48 +0530

    working on create new educator

---
+++ client/src/app/(dashboard)/admin/educators/createNewEducator/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import CreateEducatorForm from '@/components/CreateEducatorForm'
+
+const CreateNewEducator = () => {
+  return (
+      <div className="p-6">
+          
+          
+          <CreateEducatorForm/>
+      
+      </div>
+  )
+}
+
+export default CreateNewEducator
--- client/src/app/(dashboard)/admin/educators/page.tsx
+++ client/src/app/(dashboard)/admin/educators/page.tsx
@@ -2,14 +2,20 @@
 
 import React from "react";
 
+import { useRouter, usePathname } from 'next/navigation';
+
 import { useState } from "react";
 import EducatorsCardsView_forAdmin from "@/components/EducatorsCardsView_forAdmin";
 import EducatorsList_forAdmin from "@/components/EducatorsList_forAdmin";
-import { LayoutGrid, List } from "lucide-react";
+import { LayoutGrid, List, UserPlus } from "lucide-react";
 
 import EducatorCard_forAdmin from "@/components/EducatorCard_forAdmin";
 
 const Educators = () => {
+
+  const router = useRouter();
+  const pathname = usePathname()
+
   const [view, setView] = useState<"list" | "card">("list");
 
   const toggleView = () => {
@@ -21,13 +27,19 @@ const Educators = () => {
       <div className="p-6">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Educators</h1>
-          <button
+          <div className="buttons group "><button
             onClick={toggleView}
             className="p-2 bg-gray-100 rounded hover:bg-gray-200"
             title="Toggle View"
           >
             {view === "list" ? <LayoutGrid size={20} /> : <List size={20} />}
           </button>
+            <button className="ml-2 mr-2" onClick ={() => router.push(`${pathname}/createNewEducator`)}>
+            <UserPlus size={20} />
+          </button>
+        
+        </div>
+          
         </div>
 
         {view === "list" ? (
