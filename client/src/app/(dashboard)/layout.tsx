7d866ddd8a479c33fcc712551418dcb0d8d90b4b
travailbeyondexcellence <zen@paathshala.world>
2025-05-26 23:11:15 +0530

    Layout File edited!

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -22,7 +22,7 @@ function LayoutWithSidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-screen h-screen overflow-hidden">
       <aside
-        className={`sidebar fixed top-0 left-0 h-screen p-0 pl-0 w-56 z-50 bg-background transition-transform duration-300 ease-in-out border-transparent dark:border-transparent overflow-hidden bg-gray-900 light:bg-gray-300 text-gray-500 dark:text-gray-400
+        className={`sidebar fixed top-0 left-0 h-screen p-0 pl-0 w-56 z-50 transition-transform duration-300 ease-in-out border-transparent dark:border-transparent overflow-hidden bg-gray-300 dark:bg-gray-900 text-gray-700 dark:text-gray-400
    
     
         ${open ? "translate-x-0" : "-translate-x-full"}
@@ -31,9 +31,8 @@ function LayoutWithSidebar({ children }: { children: React.ReactNode }) {
         <DashboardSidebar />
       </aside>
 
-
       <div
-        className={`w-screen flex flex-col min-h-screen transition-all duration-300 ${open ? "ml-56" : "ml-0 pl-0"
+        className={`w-screen flex flex-col min-h-screen transition-all duration-300 bg-background ${open ? "ml-56" : "ml-0 pl-0"
           }`}
       >
         <DashboardNavbar />
