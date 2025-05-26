7d9fb5f906c0763e534343f053ac636aec3a5441
travailbeyondexcellence <zen@paathshala.world>
2025-05-26 23:36:09 +0530

    Trying to fix Tailwind!

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,13 @@
 // postcss.config.js
 module.exports = {
     plugins: {
+        'postcss-flexbugs-fixes': {},
+        'postcss-preset-env': {
+            autoprefixer: { flexbox: 'no-2009' },
+            stage: 3,
+            features: { 'custom-properties': false },
+        },
         '@tailwindcss/postcss': {},
-        // autoprefixer: {},
     },
 };
   
