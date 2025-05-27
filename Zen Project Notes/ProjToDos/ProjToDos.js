c98ec1e1a60bad7d6c15d9a9c3872a9fe7ec96e9
travailbeyondexcellence <zen@paathshala.world>
2025-05-27 17:49:44 +0530

    id: todo3,         category: ZensCompletedTodos,         title: "LightMode not working for the sidebar - fix it Tailwind CSS fixes",

--- Zen Project Notes/ProjToDos/ProjToDos.js
+++ Zen Project Notes/ProjToDos/ProjToDos.js
@@ -23,14 +23,5 @@ const Todos = [
         tags: ["tag1", "tag2"],
     },
 
-    {
-        id: todo3,
-        title: "LightMode not working for the sidebar - fix it Tailwind CSS fixes",
-        description: "LightMode not working for the sidebar - fix it",
-        completed: false,
-        assignedTo: "Zenith",
-        dueDate: "2025-10-01",
-        priority: "Medium",
-        tags: ["tag1", "tag2"],
-    }
+
 ]
---
+++ Zen Project Notes/ProjToDos/ZensCompletedToDos.js
@@ -0,0 +1,16 @@
+
+const ZensCompletedTodos = [
+
+    {
+        id: todo3,
+        category: ZensCompletedTodos,
+        title: "LightMode not working for the sidebar - fix it Tailwind CSS fixes",
+        description: "LightMode not working for the sidebar - fix it",
+        completed: false,
+        assignedTo: "Zenith",
+        dueDate: "2025-10-01",
+        priority: "Medium",
+        tags: ["tag1", "tag2"],
+    }
+
+]
