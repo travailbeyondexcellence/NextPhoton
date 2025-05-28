b98b4a1820f8fabb087bba436f44fa046b82c6d6
travailbeyondexcellence <zen@paathshala.world>
2025-05-28 15:48:00 +0530

    schema updated by betterAuth cli

---
+++ shared/prisma/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+// import { PrismaClient } from "../../../shared/db/index";
+
+import { prisma } from "../db"; // Import the prisma singleton from shared
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql"
+    }),
+    // Add other configurations like authentication methods (email/password, social providers) here
+    emailAndPassword: {
+        enabled: true
+    },
+    // socialProviders: { 
+    //     github: { 
+    //        clientId: process.env.GITHUB_CLIENT_ID as string, 
+    //        clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+    //     }, 
+    // }, 
+}); 
